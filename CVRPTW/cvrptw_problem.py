e21a942f97854278e96e8dc5c85337976af996c8
matib99 <40698588+matib99@users.noreply.github.com>
2019-09-03 14:35:25 +0200

    bugs fixed

--- CVRPTW/cvrptw_problem.py
+++ CVRPTW/cvrptw_problem.py
@@ -9,6 +9,17 @@ class CVRPTWProblem:
     TIME_BLOCK = 10
 
     def __init__(self, sources, costs, time_costs, capacities, dests, weights, time_windows, vehicles_num, time_blocks_num):
+
+        self.costs = costs
+        self.time_costs = time_costs
+        self.capacities = capacities
+        self.dests = dests
+        self.weights = weights
+
+        self.time_windows = time_windows
+        self.time_blocks_num = time_blocks_num
+        self.vehicles_num = vehicles_num
+
         # Merging all sources into one source.
         source = 0
         weights[source] = 0
@@ -29,21 +40,14 @@ class CVRPTWProblem:
                     out_nearest = s
             costs[source][dest] = costs[in_nearest][dest]
             costs[dest][source] = costs[dest][out_nearest]
-            time_costs[source][dest] = costs[in_nearest][dest]
-            time_costs[dest][source] = costs[dest][out_nearest]
+            time_costs[source][dest] = time_costs[in_nearest][dest]
+            time_costs[dest][source] = time_costs[dest][out_nearest]
             in_nearest_sources[dest] = in_nearest
             out_nearest_sources[dest] = out_nearest
         time_costs[source][source] = 0
-        self.costs = costs
-        self.time_costs = time_costs
-        self.capacities = capacities
-        self.dests = dests
-        self.weights = weights
+
         self.in_nearest_sources = in_nearest_sources
         self.out_nearest_sources = out_nearest_sources
-        self.time_windows = time_windows
-        self.time_blocks_num = time_blocks_num
-        self.vehicles_num = vehicles_num
 
     def get_capacity_qubo(self, capacity_const):
         dests = self.dests
@@ -58,7 +62,7 @@ class CVRPTWProblem:
                     cap_qubo.add(index, cost)
         return cap_qubo
 
-    def get_time_windows_qubo(self, value):
+    def get_time_windows_qubo(self, penalty_const, reward_const):
         tw_qubo = Qubo()
         for d in self.dests:
             tw = self.time_windows[d]
@@ -66,14 +70,14 @@ class CVRPTWProblem:
                 for v in range(self.vehicles_num):
                     index = ((v, d, t),(v, d, t))
                     if tw[0] > t or tw[1] < t:
-                        tw_qubo.add(index, value)
+                        tw_qubo.add(index, penalty_const)
                     else:
-                        tw_qubo.add(index, 0)
+                        tw_qubo.add(index, reward_const)
         return tw_qubo
 
     # z tą funkcją są pewne problemy, ponieważ siędodają zmienne od źródła a one nieco psują, więc na
     # razie je usunę
-    def get_sources_qubo(self, mi, rho, lmb):
+    def get_sources_qubo(self, order_const_m, order_const_r, penalty_const, reward_const):
         src_qubo = Qubo()
         dests = self.dests
         source = self.source
@@ -86,30 +90,38 @@ class CVRPTWProblem:
                 for v in range(self.vehicles_num):
                     in_index = ((v, source, 0), (v, dest, t))
                     out_index = ((v, dest, t), (v, source, time_blocks_num - 1))
+                    dest_index = ((v, dest, t), (v, dest, t))
 
                     # too early to arrive from source
                     if t < in_time:
-                        src_qubo.add(in_index, lmb)
+                        print(str(t) + " < in_time --> " + str(in_time))
+                        # src_qubo.add(in_index, penalty_const)
+                        src_qubo.add(dest_index, penalty_const)
+
                     else:
-                        cost = (costs[source][dest] - mi) / rho  # [0]
+                        cost = (costs[source][dest] - order_const_m) / order_const_r  # [0]
                         src_qubo.add(in_index, cost)
 
                     # to late to go back to the source
                     if time_blocks_num - t - 1 < out_time:
-                        src_qubo.add(out_index, lmb)
+                        print(str(time_blocks_num - t - 1) + " > out_time --> " + str(out_time))
+                        # src_qubo.add(out_index, penalty_const)
+                        src_qubo.add(dest_index, penalty_const)
                     else:
-                        cost = (costs[dest][source] - mi) / rho  # [t]
+                        cost = (costs[dest][source] - order_const_m) / order_const_r  # [t]
                         src_qubo.add(out_index, cost)
 
                     # visiting source can only be at the beginning and at the end
+                    src_index = ((v, source, t), (v, source, t))
                     if t != time_blocks_num - 1 and t != 0:
-                        src_index = ((v, source, t), (v, source, t))
-                        src_qubo.add(src_index, lmb)
+                        src_qubo.add(src_index, penalty_const)
+                    else:
+                        src_qubo.add(src_index, reward_const)
 
         return src_qubo
 
     def get_cvrptw_qubo(self,
-                        penalty_const, order_const_m, order_const_r, capacity_const, time_windows_const):
+                        penalty_const, reward_const, order_const_m, order_const_r, capacity_const, time_windows_const):
 
         dests = self.dests
         costs = self.costs
@@ -119,20 +131,26 @@ class CVRPTWProblem:
         vrp_qubo = Qubo()
 
         for (d1, d2) in permutations(dests, 2):
-            for t in range(time_blocks_num - 1):
-                for i in range(vehicles_num):
-                    # going from (d1) to (d2) at the time (t) with vehicle (i)
-                    # nie wiem jak dobrze nazwać te stałe od order
-                    # zamieniłem tu kolejność d1 i d2 na intuicyjniejszą, nie wiem czy będzie dobrze działać ale na moje powinno
-                    cost = (costs[d1][d2] - order_const_m)/order_const_r  # [t]
-                    var2 = (i, d2, t + time_costs[d1][d2])  # [t]
-                    var1 = (i, d1, t)
-                    vrp_qubo.add((var1, var2), cost)
+            for (t1, t2) in combinations(range(time_blocks_num), 2):
+                if t1 + time_costs[d1][d2] <= t2:
+                    for i in range(vehicles_num):
+                        # going from (d1) to (d2) at the time (t) with vehicle (i)
+                        # nie wiem jak dobrze nazwać te stałe od order
+                        # zamieniłem tu kolejność d1 i d2 na intuicyjniejszą, nie wiem czy będzie dobrze działać
+                        # ale na moje powinno
+                        cost = (costs[d1][d2] - order_const_m)/order_const_r  # [t]
+                        var2 = (i, d2, t2)  # [t]
+                        var1 = (i, d1, t1)
+                        vrp_qubo.add((var1, var2), cost)
+                        print(((var1, var2), cost))
 
-                    for dt in range(1, time_costs[d1][d2]):  # [t]
-                        # cannot go from (b) to (a) at the time (t) in (dt)
-                        var3 = (i, d2, t + dt)
-                        vrp_qubo.add((var1, var3), penalty_const)
+                else:
+                    if t1 <= t2:
+                        for i in range(vehicles_num):
+                            # cannot go from (b) to (a) at the time (t1) in (dt)
+                            var1 = (i, d1, t1)
+                            var3 = (i, d2, t2)
+                            vrp_qubo.add((var1, var3), penalty_const)
 
         # cannot be in 2 places at the same time
         for (d1, d2) in combinations(dests, 2):
@@ -158,12 +176,14 @@ class CVRPTWProblem:
                     var2 = (i2, d, t)
                     vrp_qubo.add((var1, var2), penalty_const)
 
-        # src_qubo = self.get_sources_qubo(order_const_m, order_const_r, penalty_const)
+        src_qubo = self.get_sources_qubo(order_const_m, order_const_r, penalty_const, reward_const)
         cap_qubo = self.get_capacity_qubo(capacity_const)
-        tw_qubo = self.get_time_windows_qubo(time_windows_const)
+        tw_qubo = self.get_time_windows_qubo(time_windows_const, reward_const)
 
-        # vrp_qubo.merge_with(src_qubo, 1, 1)
+        vrp_qubo.merge_with(src_qubo, 1, 1)
         vrp_qubo.merge_with(cap_qubo, 1, 1)
         vrp_qubo.merge_with(tw_qubo, 1, 1)
+
+        vrp_qubo.bound(reward_const, penalty_const)
 
         return vrp_qubo
